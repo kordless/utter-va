90687d8440bf8fd91a485bbbde878d96430983d6
Kord Campbell <kordless@gmail.com>
2013-12-27 23:04:31 -0800

    no force in pip!

--- install.sh
+++ install.sh
@@ -8,11 +8,12 @@ sudo apt-get install git -y
 sudo apt-get install python-pip -y
 
 # install flask bits via pip
-sudo pip install Flask -y
-sudo pip install flask-wtf -y
-sudo pip install flask-appconfig -y
-sudo pip install flask-bootstrap -y
+sudo pip install Flask
+sudo pip install flask-wtf
+sudo pip install flask-appconfig
+sudo pip install flask-bootstrap
 
 # check out stackgeek-vm repo
-sudo cd /root/
+sudo su
+cd /root/
 sudo git clone https://github.com/StackMonkey/stackmonkey-vm.git
