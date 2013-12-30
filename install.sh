c44429bf66225f6559e84eb04f0f6946956a0ba3
Kord Campbell <kordless@gmail.com>
2013-12-29 18:08:28 -0800

    rejigger the deployment

--- install.sh
+++ install.sh
@@ -13,50 +13,43 @@ sudo apt-get install git -y
 sudo apt-get install python-pip -y
 
 # install service and dependencies
-sudo apt-get install nginx -y
 sudo apt-get install build-essential -y
 sudo apt-get install python-dev -y
-sudo pip install uwsgi 
-
-# configure user settings
-sudo useradd -c 'uwsgi user,,,' -g www-data -d /nonexistent -s /bin/false uwsgi
+sudo apt-get install nginx -y
+sudo apt-get install uwsgi -y
+sudo apt-get install uwsgi-plugin-python -y
 
 # configure www directory
-sudo mkdir -p /var/www/stackmonkey/
+sudo mkdir -p /var/www/stackmonkey/static
 sudo usermod -a -G www-data www-data
 sudo chown -R www-data:www-data /var/www/
 sudo chmod -R g+w /var/www/stackmonkey/
 
-# configure uwsgi configs
-sudo cat <<EOF > /etc/init/uwsgi.conf
-description "uWSGI"
-start on runlevel [2345]
-stop on runlevel [06]
+# install virtual environment 
+sudo pip install virtualenv
 
-respawn
+# configure virtual environment
+sudo cd /var/www/stackmonkey/
+sudo virtualenv ./env
+sudo source env/bin/activate
 
-exec uwsgi --master --processes 4 --die-on-term --uid uwsgi --gid www-data --socket /tmp/uwsgi.sock --chmod-socket 660 --no-site --vhost --logto /var/log/uwsgi.log
-EOF
+# install flask bits via pip
+sudo pip install Flask
+sudo pip install flask-wtf
+sudo pip install flask-appconfig
+sudo pip install flask-bootstrap
 
-# set up logging for uwsgi
-sudo cat <<EOF > /etc/logrotate.d/uwsgi
-/var/log/uwsgi.log {
-    rotate 10
-    daily
-    compress
-    missingok
-    create 640 uwsgi adm
-    postrotate
-        initctl restart uwsgi >/dev/null 2>&1
-    endscript
-}
-EOF
+# install openstack libraries for python
+sudo pip install python-keystoneclient
+sudo pip install python-glanceclient
+sudo pip install python-cinderclient
+sudo pip install python-novaclient
 
-sudo touch /var/log/uwsgi.log
-sudo logrotate -f /etc/logrotate.d/uwsgi
+# exit env
+sudo deactivate
 
 # configure nginx
-sudo cat <<EOF > /etc/nginx/conf.d/stackmonkey.conf
+sudo cat <<EOF > /etc/nginx/site-available/default
 server {
     listen       80;
     server_name  stackmonkey;
@@ -67,7 +60,7 @@ server {
 
     location / {
         include uwsgi_params;
-        uwsgi_pass unix:/tmp/uwsgi.sock;
+        uwsgi_pass unix:/tmp/stackmonkey.sock;
         uwsgi_param UWSGI_PYHOME /var/www/stackmonkey/env;
         uwsgi_param UWSGI_CHDIR /var/www/stackmonkey;
         uwsgi_param UWSGI_MODULE application;
@@ -82,23 +75,20 @@ server {
     }
 }
 EOF
+sudo ln -s /etc/nginx/sites-available/stackmonkey /etc/nginx/sites-enabled/stackmonkey
+
+# setup uwsgi
+sudo cat <<EOF > /etc/uwsgi/apps-available/stackmonkey.ini
+[uwsgi]
+plugins=python
+vhost=true
+socket=/tmp/stackmonkey.sock
+EOF
+sudo ln -s /etc/uwsgi/apps-available/website.ini /etc/uwsgi/apps-enabled/website.ini
 
 # restart services
 sudo service uwsgi restart
 sudo service nginx restart
-
-# install flask bits via pip
-sudo pip install Flask
-sudo pip install flask-wtf
-sudo pip install flask-appconfig
-sudo pip install flask-bootstrap
-
-# install openstack libraries for python
-sudo pip install python-keystoneclient
-sudo pip install python-glanceclient
-sudo pip install python-cinderclient
-sudo pip install python-novaclient
-
 
 # check out stackgeek-vm repo
 sudo su
