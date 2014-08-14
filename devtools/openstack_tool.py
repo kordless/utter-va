31e1680b932c24b1fc595ff9e43e36994f56bf1c
Kord Campbell <kordless@gmail.com>
2014-08-14 11:55:56 -0700

    Troubleshooting tool

---
+++ devtools/openstack_tool.py
@@ -0,0 +1,44 @@
+import sqlite3
+from novaclient.v1_1 import client as novaclient
+from cinderclient import client as cclient
+import keystoneclient.v2_0.client as ksclient
+import glanceclient
+
+"""
+DROP TABLE IF EXISTS openstack;
+CREATE TABLE openstack (
+  id INTEGER NOT NULL,
+  authurl VARCHAR(100) NOT NULL,
+  tenantname VARCHAR(100) NOT NULL,
+  tenantid VARCHAR(100) NOT NULL,
+  osusername VARCHAR(100) NOT NULL,
+  ospassword VARCHAR(100),
+  PRIMARY KEY (id)
+);
+"""
+
+# connect to db and fetch info
+c = sqlite3.connect('../utterio.db')
+foo = c.execute('select * from openstack')
+result = foo.fetchone()
+
+# establish connection to openstack nova
+connection = novaclient.Client(
+	result[4], # username
+	result[5], # password
+	result[2], # tenant name
+	result[1], # auth URL
+	service_type="compute"
+)
+
+# proper way of getting glance image via python
+keystone = ksclient.Client(
+	auth_url = result[1], 
+	username = result[4], 
+	password = result[5], 
+	tenant_id = result[3]
+)
+
+glance_endpoint = keystone.service_catalog.url_for(service_type='image')
+
+print glance_endpoint
