3b4cfa78cefa463e1efef687d677a5fa598c289c
replay <mauro.stettler@gmail.com>
2014-09-05 19:31:49 +0300

    create separate home for service user

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@
 # define user/group name for services
 export USER="stackmonkey"
 export GROUP="stackmonkey"
-export HOME="/var/www/${USER}"
+export BASE_DIR="/var/www/${USER}"
 
 # update repos
 apt-get update -y
@@ -37,22 +37,25 @@ wget -qO /tmp/ngrok.zip https://dl.ngrok.com/linux_386/ngrok.zip
 unzip /tmp/ngrok.zip
 mv ngrok /usr/local/bin/ngrok
 
-# need to create /var/www to put service user's home there
-mkdir /var/www
-
 # add user and group to run services as
 groupadd ${GROUP}
-useradd -g ${GROUP} -m -d ${HOME} ${USER}
+useradd -g ${GROUP} -m -d /var/lib/stackmonkey ${USER}
 
 # check out the current release of utter-va
 mkdir /var/log/utterio/
 chown ${USER}:${GROUP} /var/log/utterio/
 
+# need to create /var/www to put service data in
+mkdir /var/www
+
 # clone appliance code
-sudo -u ${USER} git clone -b improve_installation https://github.com/StackMonkey/utter-va.git ${HOME}
+git clone -b improve_installation https://github.com/StackMonkey/utter-va.git ${BASE_DIR}
+
+# chown it all to the service user
+chown -R ${USER}:${GROUP} ${BASE_DIR}
 
 # install all the python requirements
-cd ${HOME}
+cd ${BASE_DIR}
 sudo -u ${USER} pip install -U -r ./requirements.txt
 
 # set vim tabs
@@ -70,8 +73,8 @@ set httpd port 5150 and
 set daemon 30
 with start delay 5
 
-check process ngrok matching "/usr/local/bin/ngrok -config ${HOME}/tunnel.conf start utterio"
-    start program = "${HOME}/tunnel.sh"
+check process ngrok matching "/usr/local/bin/ngrok -config ${BASE_DIR}/tunnel.conf start utterio"
+    start program = "${BASE_DIR}/tunnel.sh"
         as uid ${USER} and gid ${GROUP}
     stop program = "/usr/bin/killall screen"
 EOF
@@ -85,9 +88,9 @@ set daemon 30
 with start delay 5
 
 check process gunicorn with pidfile /tmp/gunicorn.pid
-    start program = "${HOME}/gunicorn.sh"
+    start program = "${BASE_DIR}/gunicorn.sh"
         as uid ${USER} and gid ${GROUP}
-    stop program = "${HOME}/gunistop.sh"
+    stop program = "${BASE_DIR}/gunistop.sh"
 EOF
 
 cat <<EOF > /etc/monit/conf.d/twitterbot
@@ -99,7 +102,7 @@ set daemon 30
 with start delay 5
 
 check process twitterbot matching "manage.py tweetstream"
-		start program = "${HOME}/tweetstream.sh"
+		start program = "${BASE_DIR}/tweetstream.sh"
         as uid ${USER} and gid ${GROUP}
 		stop program = "/bin/true"
 EOF
@@ -110,20 +113,20 @@ sleep 2
 monit monitor all
 
 # generate tokens and write into new config.py file
-sudo -u ${USER} cp ${HOME}/config.py.template ${HOME}/config.py
+sudo -u ${USER} cp ${BASE_DIR}/config.py.template ${BASE_DIR}/config.py
 SECRET_KEY=`date +%N | md5sum | cut -d' ' -f1`
 sleep 1
 CSRF_SESSION_KEY=`date +%N | md5sum | cut -d' ' -f1`
 sed -e "
 s,%SECRET_KEY%,$SECRET_KEY,g;
 s,%CSRF_SESSION_KEY%,$CSRF_SESSION_KEY,g;
-" -i ${HOME}/config.py
+" -i ${BASE_DIR}/config.py
 
 # grab the IP address of the box
 MYIP=$(/sbin/ifconfig eth0| sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p')
 
 # build the database and sync with pool operator
-sudo -u ${USER} ${HOME}/manage.py install -i $MYIP
+sudo -u ${USER} ${BASE_DIR}/manage.py install -i $MYIP
 
 # install crontab for service user to run every 15 minutes starting with a random minute
 MICROS=`date +%N`
@@ -140,22 +143,22 @@ TEN=`expr $ONE + 45`
 ELEVEN=`expr $ONE + 50`
 TWELVE=`expr $ONE + 55`
 
-cat <<EOF > ${HOME}/crontab
+cat <<EOF > ${BASE_DIR}/crontab
 # run various manage commands every 15 minutes
-$ONE,$FOUR,$SEVEN,$TEN * * * * ${HOME}/manage.py images > /dev/null 2>&1
-$ONE,$FOUR,$SEVEN,$TEN * * * * ${HOME}/manage.py flavors > /dev/null 2>&1
-$TWO,$FIVE,$EIGHT,$ELEVEN * * * * ${HOME}/manage.py salesman > /dev/null 2>&1
-$TWO,$FIVE,$EIGHT,$ELEVEN * * * * ${HOME}/manage.py marketeer > /dev/null 2>&1
-$THREE,$SIX,$NINE,$TWELVE * * * * ${HOME}/manage.py trashman > /dev/null 2>&1
+$ONE,$FOUR,$SEVEN,$TEN * * * * ${BASE_DIR}/manage.py images > /dev/null 2>&1
+$ONE,$FOUR,$SEVEN,$TEN * * * * ${BASE_DIR}/manage.py flavors > /dev/null 2>&1
+$TWO,$FIVE,$EIGHT,$ELEVEN * * * * ${BASE_DIR}/manage.py salesman > /dev/null 2>&1
+$TWO,$FIVE,$EIGHT,$ELEVEN * * * * ${BASE_DIR}/manage.py marketeer > /dev/null 2>&1
+$THREE,$SIX,$NINE,$TWELVE * * * * ${BASE_DIR}/manage.py trashman > /dev/null 2>&1
 
 # run various manage commands every 5 minutes
-$ONE,$TWO,$THREE,$FOUR,$FIVE,$SIX,$SEVEN,$EIGHT,$NINE,$TEN,$ELEVEN,$TWELVE * * * * ${HOME}/manage.py housekeeper > /dev/null 2>&1
+$ONE,$TWO,$THREE,$FOUR,$FIVE,$SIX,$SEVEN,$EIGHT,$NINE,$TEN,$ELEVEN,$TWELVE * * * * ${BASE_DIR}/manage.py housekeeper > /dev/null 2>&1
 
 # run various manage commands every 1 minute whic break that into 15 second runs
-* * * * * ${HOME}/manage.py instances -c 60 -f 15 > /dev/null 2>&1
-* * * * * ${HOME}/manage.py falconer -c 60 -f 15 > /dev/null 2>&1
+* * * * * ${BASE_DIR}/manage.py instances -c 60 -f 15 > /dev/null 2>&1
+* * * * * ${BASE_DIR}/manage.py falconer -c 60 -f 15 > /dev/null 2>&1
 EOF
-crontab -u ${USER} ${HOME}/crontab
+crontab -u ${USER} ${BASE_DIR}/crontab
 
 # finally, start downloading images
-sudo -u ${USER} ${HOME}/manage.py images
+sudo -u ${USER} ${BASE_DIR}/manage.py images
